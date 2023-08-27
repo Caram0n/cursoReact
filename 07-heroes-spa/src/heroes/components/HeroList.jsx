9bb63fb75d24c31ff6ac84059736334106dccc54
David <diazhdez182@gmail.com>
2023-08-28 00:10:43 +0100

    Fin sección 14

---
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -0,0 +1,20 @@
+import React, { useMemo } from 'react'
+import { getheroesByPublisher } from '../helpers'
+import { HeroCard } from './'
+
+export const HeroList = ({publisher}) => {
+
+    const heroes = useMemo(() => getheroesByPublisher(publisher), [publisher])
+  return (
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
+        {
+            heroes.map((hero) => (
+                <HeroCard
+                 key={hero.id}
+                 {...hero}
+                 />
+            ))
+        }
+    </div>
+  )
+}
