232951aa71ca8b2b411d69720b3aebfbd43c73bb
David <diazhdez182@gmail.com>
2023-09-28 12:47:59 +0100

    Fin sección 18

---
+++ 09-toolkit-redux/src/App.jsx
@@ -0,0 +1,36 @@
+import {useDispatch, useSelector} from 'react-redux'
+import {increment, decrement, incrementByAmount} from './store/slices/counter'
+
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  const {counter} = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <div>
+        
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <p>count is: {counter}</p>
+      <div className="card">
+        <button onClick={ () => dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={ () => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={ () => dispatch(incrementByAmount(2))}>
+          Increment +2
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
